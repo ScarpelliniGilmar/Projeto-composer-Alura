9cb38bd3c9a2af6af6639c27aebc6f4681a39dd6
Gilmar Trindade <gilmar.trindade@hotmail.com>
2020-10-24 20:41:14 -0300

    Primeiro commit

---
+++ info.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * composer install
+ * composer update
+ * composer dumpautoload
+ * autoload - files, classmap e psr-4
+ * composer require dependencia
+ * composer require --dev phpunit/phpunit
+ * composer install --no-dev
+ * vendor\bin\phpunit --version  // versão do phpunit
+ * vendor\bin\phpunit tests\variavel.php
+ *
+ *                       //Verificar identação
+ * https://www.php-fig.org/psr/psr-12/
+ * composer require --dev squizlabs/php_codesniffer
+ * vendor\bin\phpcs --standard=PSR12 src\Buscador.php
+ *
+ *                      //verificar erros de sintaxe
+ * composer require --dev phan/phan
+ * vendor\bin\phan.bat --help
+ * vendor\bin\phan --allow-polyfill-parser src\Buscador.php
+ *
+ *                              Utilizando scripts
+ * https://getcomposer.org/doc/articles/scripts.md
+ *
+ *
+ */
